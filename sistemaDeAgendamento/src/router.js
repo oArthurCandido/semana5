3914742a2079fc1889234208a9d7217211e1a281
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-05-31 19:55:46 -0300

    commiting right before the error

--- sistemaDeAgendamento/src/router.js
+++ sistemaDeAgendamento/src/router.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Okay' });
+routes.get('/', async (req, res) => {
+  const user = {
+    name: 'Douglas',
+    email: 'douglas@email.com',
+    password_hash: '123456'
+  };
+  return res.json(user);
 });
 
 export default routes;
