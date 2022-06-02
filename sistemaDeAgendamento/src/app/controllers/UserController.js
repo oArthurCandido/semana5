5043a8dc286721a52d7c4619e8dd0006d7cac162
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-02 11:31:48 -0300

    Commiting in the end of class 12

--- sistemaDeAgendamento/src/app/controllers/UserController.js
+++ sistemaDeAgendamento/src/app/controllers/UserController.js
@@ -18,8 +18,33 @@ class UserController {
     });
   }
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ message: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email != user.email) {
+      const userExists = await User.findOne({
+        where: { email }
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuário já cadastrado' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        message: 'Senha não confere.'
+      });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
   }
 }
 
