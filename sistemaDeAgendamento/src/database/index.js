56f64f47027258ce65f155295e0c5ce70602498d
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-03 23:25:02 -0300

    commiting in the end of class 21, everything ok

--- sistemaDeAgendamento/src/database/index.js
+++ sistemaDeAgendamento/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -9,12 +10,22 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb+srv://sistemadeagendamento:sistemadeagendamento@cluster0.hfbv7.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
   }
 }
 
